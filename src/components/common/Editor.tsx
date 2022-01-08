fd02bbc5a1b8f4c84a823b4bbc85ca5bbafcb8a6
rik9228 <rank228130@gmail.com>
2022-01-08 20:51:44 +0900

    add: Editorコンポーネント, 新規作成ページ

---
+++ src/components/common/Editor.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import { Box, Heading } from "@chakra-ui/react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "quill-mention";
+import "quill-mention/dist/quill.mention.css";
+import { marked } from "marked";
+
+const atValues = [
+  { id: 1, value: "Fredrik Sundqvist" },
+  { id: 2, value: "Patrik Sjölin" },
+];
+
+const modules = {
+  toolbar: [
+    [{ font: [] }],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ["bold", "italic", "underline", "strike"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    ["blockquote", "code-block"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ indent: "-1" }, { indent: "+1" }, { align: [] }],
+    ["link", "image", "video"],
+    ["clean"],
+  ],
+
+  mention: {
+    allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
+    mentionDenotationChars: ["@", "#"],
+    source: function (searchTerm, renderList, mentionChar) {
+      let values;
+
+      if (mentionChar === "@") {
+        values = atValues;
+      } else {
+        values = atValues;
+      }
+
+      if (searchTerm.length === 0) {
+        renderList(values, searchTerm);
+      } else {
+        const matches = [];
+        for (let i = 0; i < values.length; i++)
+          if (~values[i].value.toLowerCase().indexOf(searchTerm.toLowerCase()))
+            matches.push(values[i]);
+        renderList(matches, searchTerm);
+      }
+    },
+  },
+};
+
+export const Editor = () => {
+  const defaultTemplate: string = `
+  <h2><strong>## 概要</strong></h2>
+  <br/>
+  <h2><strong>## 工夫したところ</strong></h2>
+  <br/>
+  <h2><strong>## 気になっているところ</strong></h2>
+  <br/>
+  <h2><strong>## その他</strong></h2>
+  `;
+  const [value, setValue] = useState(defaultTemplate);
+
+  return (
+    <>
+      <ReactQuill
+        theme="snow"
+        modules={modules}
+        value={value}
+        onChange={setValue}
+        placeholder="Content goes here..."
+      />
+      <Box>
+        <Heading background={"#ccc"} marginTop={10} padding={2} fontSize={"xl"}>
+          プレビュー
+        </Heading>
+        <Box border={"1px solid #ccc"} padding={5}>
+          <span dangerouslySetInnerHTML={{ __html: marked(value) }} />
+        </Box>
+      </Box>
+    </>
+  );
+};
--- src/pages/works/new.tsx
+++ src/pages/works/new.tsx
@@ -1,3 +1,113 @@
+import {
+  Button,
+  Heading,
+  Stack,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+} from "@chakra-ui/react";
+import { Layout } from "src/components/common/Layout";
+import { Editor } from "src/components/common/Editor";
+import { LgContainer } from "src/components/custom/LgContainer";
+
 export default function New() {
-  return <p>新規作品投稿ページ</p>;
+  return (
+    <Layout title="タイトル">
+      <LgContainer>
+        <Heading textAlign={"center"}>プロフィール</Heading>
+        <Box marginTop={["56px", "80px"]}>
+          <Stack
+            direction="column"
+            justifyContent={"space-between"}
+            spacing={8}
+          >
+            <FormControl display={"flex"} alignItems={"center"}>
+              <FormLabel
+                htmlFor="title"
+                marginBottom={0}
+                whiteSpace={"nowrap"}
+                minWidth={"180px"}
+              >
+                タイトル：
+              </FormLabel>
+              <Input
+                id="title"
+                type="text"
+                variant={"unstyled"}
+                borderBottom={"1px"}
+                borderRadius={"none"}
+                width={"100%"}
+                paddingY={"5px"}
+                placeholder="タイトルが入ります。"
+              />
+            </FormControl>
+            <FormControl display={"flex"} alignItems={"center"}>
+              <FormLabel
+                htmlFor="share"
+                marginBottom={0}
+                whiteSpace={"nowrap"}
+                minWidth={"180px"}
+              >
+                デザイン共有URL：
+              </FormLabel>
+              <Input
+                id="share"
+                type="text"
+                variant={"unstyled"}
+                borderBottom={"1px"}
+                borderRadius={"none"}
+                width={"100%"}
+                paddingY={"5px"}
+                placeholder="http://example.com"
+              />
+            </FormControl>
+            <FormControl display={"flex"} alignItems={"center"}>
+              <FormLabel
+                htmlFor="img"
+                marginBottom={0}
+                whiteSpace={"nowrap"}
+                minWidth={"180px"}
+              >
+                デザインカンプ画像：
+              </FormLabel>
+              <Input
+                id="img"
+                type="text"
+                variant={"unstyled"}
+                borderBottom={"1px"}
+                borderRadius={"none"}
+                width={"100%"}
+                paddingY={"5px"}
+                placeholder="http://example.com"
+              />
+            </FormControl>
+          </Stack>
+          <Heading
+            as="h3"
+            marginTop={10}
+            marginBottom={5}
+            _before={{
+              content: `"■"`,
+            }}
+            fontSize={"2xl"}
+          >
+            相談内容
+          </Heading>
+          <Editor></Editor>
+        </Box>
+        <Button
+          colorScheme={"teal"}
+          width={"75%"}
+          display={"block"}
+          marginX={"auto"}
+          marginTop={["56px", "80px"]}
+          padding={5}
+          height={"100%"}
+        >
+          確定
+        </Button>
+      </LgContainer>
+    </Layout>
+  );
 }
