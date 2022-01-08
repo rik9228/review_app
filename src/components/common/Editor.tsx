657a416f5bd243513fb3760c697d7d263515343c
rik9228 <rank228130@gmail.com>
2022-01-08 21:41:21 +0900

    fix: Editor

--- src/components/common/Editor.tsx
+++ src/components/common/Editor.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
-import { Box, Heading } from "@chakra-ui/react";
-import ReactQuill from "react-quill";
+import { useState } from "react";
 import "react-quill/dist/quill.snow.css";
-import "quill-mention";
-import "quill-mention/dist/quill.mention.css";
+const ReactQuill =
+  typeof window === "object" ? require("react-quill") : () => false;
 
-const atValues = [
-  { id: 1, value: "Fredrik Sundqvist" },
-  { id: 2, value: "Patrik Sjölin" },
-];
+// import "quill-mention";
+// import "quill-mention/dist/quill.mention.css";
+
+// const atValues = [
+//   { id: 1, value: "Fredrik Sundqvist" },
+//   { id: 2, value: "Patrik Sjölin" },
+// ];
 
 const modules = {
   toolbar: [
@@ -23,61 +24,37 @@ const modules = {
     ["link", "image", "video"],
     ["clean"],
   ],
-
-  mention: {
-    allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
-    mentionDenotationChars: ["@", "#"],
-    source: function (searchTerm, renderList, mentionChar) {
-      let values;
-
-      if (mentionChar === "@") {
-        values = atValues;
-      } else {
-        values = atValues;
-      }
-
-      if (searchTerm.length === 0) {
-        renderList(values, searchTerm);
-      } else {
-        const matches = [];
-        for (let i = 0; i < values.length; i++)
-          if (~values[i].value.toLowerCase().indexOf(searchTerm.toLowerCase()))
-            matches.push(values[i]);
-        renderList(matches, searchTerm);
-      }
-    },
-  },
+  // mention: {
+  //   allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
+  //   mentionDenotationChars: ["@", "#"],
+  //   source: function (searchTerm, renderItem, mentionChar) {
+  //     let values;
+  //     if (mentionChar === "@" || mentionChar === "#") {
+  //       values = atValues;
+  //     }
+  //     if (searchTerm.length === 0) {
+  //       renderItem(values, searchTerm);
+  //     } else {
+  //       const matches = [];
+  //       for (let i = 0; i < values.length; i++)
+  //         if (~values[i].value.toLowerCase().indexOf(searchTerm.toLowerCase()))
+  //           matches.push(values[i]);
+  //       renderItem(matches, searchTerm);
+  //     }
+  //   },
+  // },
 };
 
 export const Editor = () => {
-  const defaultTemplate: string = `
-  <h2><strong>## 概要</strong></h2>
-  <br/>
-  <h2><strong>## 工夫したところ</strong></h2>
-  <br/>
-  <h2><strong>## 気になっているところ</strong></h2>
-  <br/>
-  <h2><strong>## その他</strong></h2>
-  `;
-  const [value, setValue] = useState(defaultTemplate);
+  const [value, setValue] = useState("");
+  console.log(value);
 
   return (
-    <>
-      <ReactQuill
-        theme="snow"
-        modules={modules}
-        value={value}
-        onChange={setValue}
-        placeholder="Content goes here..."
-      />
-      <Box>
-        <Heading background={"#ccc"} marginTop={10} padding={2} fontSize={"xl"}>
-          プレビュー
-        </Heading>
-        {/* <Box border={"1px solid #ccc"} padding={5}>
-          <span dangerouslySetInnerHTML={{ __html: marked(value) }} />
-        </Box> */}
-      </Box>
-    </>
+    <ReactQuill
+      modules={modules}
+      theme="snow"
+      onChange={setValue}
+      placeholder="Content goes here..."
+    />
   );
 };
