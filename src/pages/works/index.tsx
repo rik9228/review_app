c2b9b448ff1f5ff9f9097c90af5f8b64e9ea3563
rik9228 <rank228130@gmail.com>
2022-01-09 17:55:32 +0900

    add: Layout

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,3 +1,20 @@
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
+import { Layout } from "src/components/common/Layout";
+import { LgContainer } from "src/components/custom/LgContainer";
+
 export default function Works() {
-  return <p>作品一覧ページ</p>;
+  return (
+    <Layout title="作品一覧">
+      <LgContainer>
+        <Heading textAlign={"center"}>投稿作品一覧</Heading>
+      </LgContainer>
+    </Layout>
+  );
 }
