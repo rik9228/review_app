86830d1aecb775629d57d934678d6db642fa39e7
rik9228 <rank228130@gmail.com>
2022-01-12 00:48:40 +0900

    add: SmContainer

---
+++ src/components/custom/SmContainer.tsx
@@ -0,0 +1,15 @@
+import { Container, ContainerProps } from "@chakra-ui/react";
+import { FC } from "react";
+
+export const SmContainer: FC<ContainerProps> = (props) => {
+  return (
+    <Container
+      maxW={"650px"}
+      margin={"0 auto"}
+      marginTop={{ sm: "80px", md: "120px" }}
+      width={"calc(100% - 16px * 2)"}
+      padding={0}
+      {...props}
+    />
+  );
+};
