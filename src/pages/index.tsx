06959e25450adbfefb7aa8ea681da95c9c890fcc
rik9228 <rank228130@gmail.com>
2022-01-13 12:38:40 +0900

    add: 作品詳細ページの作成（共通レイアウト）

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Layout } from "../components/common/Layout";
 import { Button, Heading, Flex, Text } from "@chakra-ui/react";
-import Link from "next/link";
 import { MdContainer } from "src/components/custom/MdContainer";
+import Link from "next/link";
 
 export default function Home() {
   return (
--- src/pages/works/[workId].tsx
+++ src/pages/works/[workId].tsx
@@ -1,3 +1,14 @@
+import { Box, Heading, Stack } from "@chakra-ui/react";
+import { Layout } from "src/components/common/Layout";
+import { LgContainer } from "src/components/custom/LgContainer";
+
 export default function Work() {
-  return <p>作品詳細ページ</p>;
+  return (
+    <Layout title="作品詳細">
+      <LgContainer>
+        <Heading textAlign={"center"}>作品詳細</Heading>
+        <Box marginTop={["56px", "80px"]}></Box>
+      </LgContainer>
+    </Layout>
+  );
 }
